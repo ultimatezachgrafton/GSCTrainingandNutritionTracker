9a020e29bb77dcf19117499e23372aaf1343dce5
Zach Grafton <zachgrafton@gmail.com>
2019-10-05 18:26:07 -0700

    OnClick event

--- app/src/main/java/zachg/gsctrainingandnutritiontracker/ui/fragments/LoginFragment.java
+++ app/src/main/java/zachg/gsctrainingandnutritiontracker/ui/fragments/LoginFragment.java
@@ -67,7 +67,7 @@ public class LoginFragment extends Fragment {
         return v;
     }
 
-    // auth
+    // authenticate
     public void authenticate() {
         if (email != null && password != null) {
             loginViewModel.onClick(email, password);
