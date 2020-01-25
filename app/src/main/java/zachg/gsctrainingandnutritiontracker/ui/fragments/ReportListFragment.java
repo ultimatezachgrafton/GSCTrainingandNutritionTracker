94da3b947f8b032f9374e932ec04a4489f80fb9f
Zach Grafton <zachgrafton@gmail.com>
2020-01-24 22:11:44 -0800

    ReportListAdapter

--- app/src/main/java/zachg/gsctrainingandnutritiontracker/ui/fragments/ReportListFragment.java
+++ app/src/main/java/zachg/gsctrainingandnutritiontracker/ui/fragments/ReportListFragment.java
@@ -1,4 +1,140 @@
 package zachg.gsctrainingandnutritiontracker.ui.fragments;
 
-public class ReportListFragment {
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentSnapshot;
+
+import zachg.gsctrainingandnutritiontracker.R;
+import zachg.gsctrainingandnutritiontracker.databinding.FragmentAdminUserListBinding;
+import zachg.gsctrainingandnutritiontracker.models.Report;
+import zachg.gsctrainingandnutritiontracker.models.User;
+import zachg.gsctrainingandnutritiontracker.ui.activities.SingleFragmentActivity;
+import zachg.gsctrainingandnutritiontracker.ui.adapters.ReportListAdapter;
+import zachg.gsctrainingandnutritiontracker.ui.adapters.UserListAdapter;
+import zachg.gsctrainingandnutritiontracker.viewmodels.AdminUserListViewModel;
+import zachg.gsctrainingandnutritiontracker.viewmodels.ReportListViewModel;
+
+public class ReportListFragment extends Fragment {
+
+    private FragmentReportListBinding binding;
+    private ReportListViewModel reportListViewModel;
+    private ReportListAdapter reportListAdapter;
+
+    private FirebaseAuth auth = FirebaseAuth.getInstance();
+    public String TAG = "ReportListFragment";
+
+    User currentUser = new User();
+
+    public ReportListFragment(User user) {
+        this.currentUser = user;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        binding = FragmentReportListBinding.inflate(inflater, container, false);
+        final View v = binding.getRoot();
+        binding.setUser(currentUser);
+
+        // Gets ViewModel instance to observe  LiveData
+        binding.setModel(reportListViewModel);
+        reportListViewModel = ViewModelProviders.of(getActivity()).get(ReportListViewModel.class);
+        reportListViewModel.init(currentUser);
+
+        reportListViewModel.getReports().observe(this, new Observer<FirestoreRecyclerOptions<Report>>() {
+            @Override
+            public void onChanged(@Nullable FirestoreRecyclerOptions<Report> reports) {
+                initRecyclerView(reports);
+                reportListAdapter.startListening();
+            }
+        });
+
+        reportListViewModel.getIsUpdating().observe(this, new Observer<Boolean>() {
+            @Override
+            public void onChanged(@Nullable Boolean aBoolean) {
+                if (!aBoolean) {
+                    binding.rvReport.smoothScrollToPosition(reportListViewModel.getReports().getValue().getSnapshots().size() - 1);
+                }
+            }
+
+        });
+
+        return v;
+    }
+
+    private void initRecyclerView(FirestoreRecyclerOptions<Report> reports) {
+        reportListAdapter = new ReportListAdapter(reports);
+        binding.rvReport.setAdapter(reportListAdapter);
+
+        binding.rvReport.setHasFixedSize(true);
+        binding.rvReport.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        reportListAdapter.setOnItemClickListener(new ReportListAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(DocumentSnapshot documentSnapshot, int position) {
+                currentUser = reportListViewModel.onItemClicked(documentSnapshot, position);
+                // Goes to client's profile fragment_report_list
+                SingleFragmentActivity.fm.beginTransaction().replace(R.id.fragment_container,
+                        new AdminClientProfileFragment(currentUser)).addToBackStack(null).commit();
+            }
+        });
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+
+    public void onStop() {
+        super.onStop();
+        reportListAdapter.stopListening();
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.admin_menu, menu);
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.bAddNewClient:
+                SingleFragmentActivity.fm.beginTransaction().replace(R.id.fragment_container,
+                        new RegisterFragment()).addToBackStack(null).commit();
+                return true;
+            case R.id.bInbox:
+                Intent sendIntent = new Intent(Intent.ACTION_VIEW);
+                sendIntent.setData(Uri.parse("sms:"));
+                startActivity(sendIntent);
+                return true;
+            case R.id.bLogout:
+                auth.signOut();
+                SingleFragmentActivity.fm.beginTransaction().replace(R.id.fragment_container,
+                        new LoginFragment()).addToBackStack(null).commit();
+                Toast.makeText(getActivity(), "Logged out", Toast.LENGTH_SHORT).show();
+                return true;
+        } return true;
+    }
 }
--- app/src/main/java/zachg/gsctrainingandnutritiontracker/viewmodels/ReportListViewModel.java
+++ app/src/main/java/zachg/gsctrainingandnutritiontracker/viewmodels/ReportListViewModel.java
@@ -1,4 +1,42 @@
 package zachg.gsctrainingandnutritiontracker.viewmodels;
 
-public class ReportListViewModel {
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.firebase.firestore.DocumentSnapshot;
+
+import zachg.gsctrainingandnutritiontracker.models.Report;
+import zachg.gsctrainingandnutritiontracker.models.User;
+import zachg.gsctrainingandnutritiontracker.repositories.FirestoreRepository;
+
+public class ReportListViewModel extends ViewModel {
+
+    private FirestoreRepository repo = new FirestoreRepository();
+    private User currentUser = new User();
+    private MutableLiveData<FirestoreRecyclerOptions<Report>> reports = new MutableLiveData<>();
+    private MutableLiveData<Boolean> isUpdating = new MutableLiveData<>();
+    public String TAG = "ReportListViewModel";
+
+    public void init(Report report) {
+        this.currentUser = user;
+        repo = FirestoreRepository.getInstance();
+        reports.setValue(repo.getReportsByUser(currentUser));
+    }
+
+    public MutableLiveData<FirestoreRecyclerOptions<Report>> getReports() {
+        return reports;
+    }
+
+    public MutableLiveData<Boolean> getIsUpdating() {
+        return isUpdating;
+    }
+
+    public Report onItemClicked(DocumentSnapshot documentSnapshot, int position) {
+        // Fetches currentUser
+        Report currentReport = documentSnapshot.toObject(Report.class);
+        String id = documentSnapshot.getId();
+        String path = documentSnapshot.getReference().getPath();
+        return currentReport;
+    }
 }
